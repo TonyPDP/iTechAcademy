ce7a65b2190f48ada4ceb3f7be4133173bcfe8a5
TonyPDP <fortest0603@gmail.com>
2025-06-21 23:05:44 +0500

    dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import search from '../../../public/search.png';
 const Dashboard = () => {
   return (
     <div>
-      <h1>Welcome back, Tony 👋</h1>
+      <div className="flex">
+        <h1>Welcome back, Tony 👋</h1>
+        <div className="relative flex items-center">
+          <input
+            type="search"
+            placeholder="Search..."
+            className="border border-gray-300 rounded-md p-2 pl-8"
+          />
+          <img
+            src={search}
+            alt="Search Icon"
+            className="w-5 h-5 absolute left-2 top-1/2 transform -translate-y-1/2 pointer-events-none"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
