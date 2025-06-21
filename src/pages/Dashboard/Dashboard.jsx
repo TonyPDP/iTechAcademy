e9dc26b90676eac16ce3104792aa32b17285de93
TonyPDP <fortest0603@gmail.com>
2025-06-21 23:42:31 +0500

    navbar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,24 +1,13 @@
-import search from '../../../public/search.png';
+import Navbar from "../Navbar/Navbar"
+
 const Dashboard = () => {
   return (
     <div>
-      <div className="flex">
-        <h1>Welcome back, Tony 👋</h1>
-        <div className="relative flex items-center">
-          <input
-            type="search"
-            placeholder="Search..."
-            className="border border-gray-300 rounded-md p-2 pl-8"
-          />
-          <img
-            src={search}
-            alt="Search Icon"
-            className="w-5 h-5 absolute left-2 top-1/2 transform -translate-y-1/2 pointer-events-none"
-          />
-        </div>
+      <div>
+        <Navbar />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
