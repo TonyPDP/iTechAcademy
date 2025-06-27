23719d223a14d78c0bd70f1686848bdd94af01ff
oqila1107 <qarchigayevad@gmail.com>
2025-06-27 15:39:18 +0500

    add muyass file

--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Oqila from './Oqila(Group UI)/Oqila'
+import Muyassar from './Muyassar(Edit (First part))/muyass'
 
 
 const Groups = () => {
@@ -8,6 +9,7 @@ const Groups = () => {
       <h2>Groups</h2>
       <p>Manage student groups and their details.</p>
       <Oqila/>
+      <Muyassar/>
     </div>
   )
 }
